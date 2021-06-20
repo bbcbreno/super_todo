9e612495c3c7185730b8790c76db382fee390b82
Breno Caldeira <breno.caldeira@gmail.com>
2021-06-20 19:10:30 -0300

    logic [buggy] for sign in page with firebase

--- lib/presentation/sign_in/widgets/sign_in_form.dart
+++ lib/presentation/sign_in/widgets/sign_in_form.dart
@@ -8,9 +8,30 @@ class SignInForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<SignInFormBloc, SignInFormState>(
-      listener: (context, state) {},
+      listener: (context, state) {
+        state.authFailureOrSuccessOption.fold(
+            () {},
+            (either) => either.fold((failure) {
+                  final snackBar = SnackBar(
+                      content: Text(
+                    failure.map(
+                      cancelledByUser: (_) => 'Cancelled',
+                      serverError: (_) => 'Server error',
+                      emailAlreadyInUse: (_) => 'Email already in use',
+                      invalidEmailAndPasswordCombination: (_) =>
+                          'Invalid email or password',
+                    ),
+                  ));
+
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                }, (_) {
+                  // TODO: navigate success
+                }));
+      },
       builder: (context, state) {
         return Form(
+          autovalidateMode: AutovalidateMode.always,
+          // autovalidate: state.showErrorMessages,
           child: ListView(
             // ignore: prefer_const_literals_to_create_immutables
             children: [
@@ -21,20 +42,50 @@ class SignInForm extends StatelessWidget {
               ),
               const SizedBox(height: 8),
               TextFormField(
+                // TODO: fix non null safety
+                initialValue: 'this work?',
                 decoration: const InputDecoration(
                   prefixIcon: Icon(Icons.email),
                   labelText: 'Email',
                 ),
                 autocorrect: false,
+                onChanged: (value) => context.read<SignInFormBloc>().add(
+                      SignInFormEvent.emailChanged(value),
+                    ),
+                validator: (_) => context
+                    .read<SignInFormBloc>()
+                    .state
+                    .emailAddress
+                    .value
+                    .fold(
+                        (f) => f.maybeMap(
+                            invalidEmail: (_) => 'Invalid Email',
+                            orElse: () => null),
+                        (_) => null),
               ),
               const SizedBox(height: 8),
               TextFormField(
+                // TODO: fix non null safety
+                initialValue: 'this work?',
                 decoration: const InputDecoration(
                   prefixIcon: Icon(Icons.lock),
                   labelText: 'Password',
                 ),
                 autocorrect: false,
                 obscureText: true,
+                onChanged: (value) => context.read<SignInFormBloc>().add(
+                      SignInFormEvent.passwordChanged(value),
+                    ),
+                validator: (_) => context
+                    .read<SignInFormBloc>()
+                    .state
+                    .password
+                    .value
+                    .fold(
+                        (f) => f.maybeMap(
+                            shortPassword: (_) => 'Short Password',
+                            orElse: () => null),
+                        (_) => null),
               ),
               const SizedBox(height: 8),
               Row(
@@ -42,13 +93,19 @@ class SignInForm extends StatelessWidget {
                 children: [
                   Expanded(
                     child: TextButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        context.read<SignInFormBloc>().add(const SignInFormEvent
+                            .signInWithEmailAndPasswordPressed());
+                      },
                       child: const Text('SIGN IN'),
                     ),
                   ),
                   Expanded(
                     child: TextButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        context.read<SignInFormBloc>().add(const SignInFormEvent
+                            .registerWithEmailAndPasswordPressed());
+                      },
                       child: const Text('REGISTER'),
                     ),
                   ),
@@ -66,7 +123,11 @@ class SignInForm extends StatelessWidget {
                     Colors.lightBlue,
                   ),
                 ),
-                onPressed: () {},
+                onPressed: () {
+                  context
+                      .read<SignInFormBloc>()
+                      .add(const SignInFormEvent.signInWithGooglePressed());
+                },
                 child: const Text('SIGN IN WITH GOOGLE'),
               ),
             ],
